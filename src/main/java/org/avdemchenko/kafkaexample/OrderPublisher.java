397613002314bb63fab33a7b8513b513b4090461
Oleksii Demchenko <oleksii.demchenko@leovegas.com>
2023-01-15 12:27:50 +0200

    Add produces and consumer properties

---
+++ src/main/java/org/avdemchenko/kafkaexample/OrderPublisher.java
@@ -0,0 +1,19 @@
+package org.avdemchenko.kafkaexample;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OrderPublisher {
+
+    @Value("${app.kafka.orders-topic}")
+    private String topic;
+
+    private final KafkaTemplate<String, Order> orderKafkaTemplate;
+
+}
